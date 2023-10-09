b573135e3162f87fd1bf758a0406efa7c9e4bb74
lajunta <lajunta@qq.com>
2023-10-09 18:30:05 +0800

    use args not toml file

--- client/main.go
+++ client/main.go
@@ -8,8 +8,6 @@ import (
 	"os/exec"
 	"strconv"
 	"strings"
-
-	"github.com/BurntSushi/toml"
 )
 
 type HostInfo struct {
@@ -27,23 +25,35 @@ var (
 	computerName string
 	ip           string
 	num          string
-	inum         int
+	inum         uint64
 	err          error
+	argsize      int
+	room         string
 )
 
 func parseArgs() {
-	size := len(os.Args)
-	if size != 2 {
-		log.Fatalln("参数不对, 使用: getip 3")
+	argsize = len(os.Args)
+	if argsize != 3 {
+		log.Fatalln("参数不对, 需要2个参数,第1为电脑室编号,第2为电脑编号")
 	}
-	num = os.Args[1]
-	inum, err = strconv.Atoi(num)
+
+	num = os.Args[2]
+	inum, err = strconv.ParseUint(num, 10, 32)
+
 	if err != nil {
 		log.Fatalln("必须是数字")
 	}
 	if inum < 1 {
 		log.Fatalln("数字必须大于1")
 	}
+
+	room = os.Args[1]
+	hi.DNS1 = "10.2.104.18"
+	hi.DNS2 = "114.114.114.114"
+	hi.GateWay = fmt.Sprintf("192.168.%s.254", room)
+	hi.IPPrefix = fmt.Sprintf("192.168.%s.", room)
+	hi.NetMask = "255.255.255.0"
+	hi.NamePrefix = "st"
 }
 
 func makeHost() {
@@ -55,16 +65,16 @@ func makeHost() {
 	}
 }
 
-func parseToml() {
-	_, err := toml.DecodeFile("hostinfo.toml", &hi)
-	if err != nil {
-		log.Fatalln("不能有解析配置文件")
-	}
-}
+// func parseToml() {
+// 	_, err := toml.DecodeFile("hostinfo.toml", &hi)
+// 	if err != nil {
+// 		log.Fatalln("不能有解析配置文件")
+// 	}
+// }
 
 func init() {
 	parseArgs()
-	parseToml()
+	// parseToml()
 }
 
 func main() {
@@ -87,25 +97,32 @@ func findCard() {
 }
 
 func setNic() {
+	if room == "0" {
+		dhcp := fmt.Sprintf("netsh interface ip set address %s dhcp", card.Name)
+		mycmd(dhcp)
+		dns := fmt.Sprintf("netsh interface ip set dns name=%s dhcp", card.Name)
+		mycmd(dns)
+		return
+	}
 	cmdIP := fmt.Sprintf("netsh interface ip set address name=%s static %s %s %s", card.Name, ip, hi.NetMask, hi.GateWay)
 	cmdDNS1 := fmt.Sprintf("netsh interface ip set dns name=%s static %s", card.Name, hi.DNS1)
 	cmdDNS2 := fmt.Sprintf("netsh interface ip add dns name=%s %s index=2", card.Name, hi.DNS2)
-	cmd := exec.Command("cmd", "/C", cmdIP)
-	cmd.Run()
-	cmd = exec.Command("cmd", "/C", cmdDNS1)
-	cmd.Run()
-	cmd = exec.Command("cmd", "/C", cmdDNS2)
-	cmd.Run()
+	mycmd(cmdIP)
+	mycmd(cmdDNS1)
+	mycmd(cmdDNS2)
 }
 
 func setName() {
 	oldName, _ := exec.Command("cmd", "/C", "hostname").Output()
 	nameStr := strings.TrimSuffix(string(oldName), "\r\n")
 	cmdName := fmt.Sprintf("WMIC computersystem where caption='%s' rename %s", nameStr, computerName)
-	cmd := exec.Command("cmd", "/C", cmdName)
-	err = cmd.Run()
+	err = mycmd(cmdName)
 	if err != nil {
 		log.Println(err.Error())
 	}
+}
 
+func mycmd(str string) error {
+	c := exec.Command("cmd","/C",str)
+	return c.Run()
 }
